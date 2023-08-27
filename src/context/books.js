27deff6162441dd0fffdbd6d3d803dbfb235fef6
Suayb Kardas <kardassuayb@gmail.com>
2023-08-28 01:05:13 +0300

    using useCallback hook

--- src/context/books.js
+++ src/context/books.js
@@ -1,4 +1,4 @@
-import { useState, createContext } from "react";
+import { useState, createContext, useCallback } from "react";
 import axios from "axios";
 
 const BooksContext = createContext();
@@ -6,10 +6,10 @@ const BooksContext = createContext();
 const Provider = ({ children }) => {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get("http://localhost:3001/books");
     setBooks(response.data);
-  };
+  }, []);
 
   const createBook = async (title) => {
     const response = await axios.post("http://localhost:3001/books", {
